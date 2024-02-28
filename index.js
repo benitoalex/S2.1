dbb4494cebc1fd062cd22e4d080254a17065198f
Mac Pro <a@posteo.es>
2024-02-28 12:44:37 +0100

    Exercici 1.5 Nivell 2 Exercici 5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const array = [13, 7, 8, 21];
+const array = [1, 3, 7, 10, 15, 17, 11, 5, 8, 12, 9];
 
-const suma = array.reduce((acumulador, numero) => acumulador + numero, 0);
+const result = array.filter(numero => numero >= 10)
+    .map(numero => numero * 2)
+    .reduce((acumulador, numero) => acumulador + numero, 0);
 
 
-console.log("la suma total de los numeros es:", suma);
+console.log(result);
