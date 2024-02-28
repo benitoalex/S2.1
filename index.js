5dd4862aa8635d3987a44669d74a1ce825896aac
Mac Pro <a@posteo.es>
2024-02-28 13:38:16 +0100

    Exercici 1.7 Nivell 1 Exercici 3

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const promesa = new Promise ((resolve,reject) =>{
-    setTimeout (() => {
-        resolve("Hola mundo");
-    }, 2000);
-});
+const promesa = (input) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (input === "Hola") {
+                resolve("La promesa se ha resuelto");
+            } else {
+                reject("La promesa ha sido rechazada")
+            }
+        }, 2000);
+    });
+}
 
-promesa.then((resultado)=> {
-    console.log(resultado);
-})
+promesa("Hola")
+    .then((resultado) => {
+        console.log(resultado);
+    })
+    .catch((error) =>{
+        console.log(error);
+    })
